9ab551d2020f2e25431a5867c40d5878b59bbb23
Tamara Stanikevska <tamara.stanikevska@asseco-see.mk>
2023-12-03 23:56:38 +0100

    Implement account service [client]

---
+++ client/src/app/shared/model/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    email: string;
+    username: string;
+    token: string;
+}
+
+export interface UserInfo {
+    firstName: string;
+    lastName: string;
+    address: string;
+}
