cfe4146a8616f8b67aacbd0f4feab94f7be941cd
Tamara Stanikevska <tamara.stanikevska@asseco-see.mk>
2023-12-11 23:44:54 +0100

    Add basket total [client]

--- client/src/app/home/components/nav-bar/nav-bar.component.ts
+++ client/src/app/home/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
+import { BasketService } from 'src/app/basket/service/basket.service';
 import { Component } from '@angular/core';
 import { AccountService } from 'src/app/account/service/account.service';
+import { ProductInBasket } from 'src/app/shared/model/basket';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,5 +9,11 @@ import { AccountService } from 'src/app/account/service/account.service';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-  constructor(public accountService: AccountService) {}
+  
+  constructor(public accountService: AccountService, public basketService: BasketService) {}
+
+  getCount(items: ProductInBasket[]) {
+    return items.reduce((sum, item) => sum + item.quantity, 0);
+  }
+
 }
